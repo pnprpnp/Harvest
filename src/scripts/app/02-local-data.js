c4ec0a8d04259b9b04a156eda6fbf0b83269dd62
pnprpnp <pukapucapuka@gmail.com>
2026-09-02 11:54:51 +0900

    Speed up harvest pallet selection

--- src/scripts/app/02-local-data.js
+++ src/scripts/app/02-local-data.js
@@ -1063,6 +1063,7 @@ function invalidatePlantingEventStateCache(){
   plantingEventStateCacheKey = "";
   plantingDateByPalletCache.clear();
   plantingStateByPalletCache.clear();
+  invalidateHarvestAvailabilityStateCache();
   invalidatePlantingAllowedPalletSetCache();
 }
 
